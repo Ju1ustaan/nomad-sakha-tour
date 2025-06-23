ac692a97d62e5de960725fedf0117c4c90f2ff90
luijulus <luijulus@gmail.com>
2025-06-23 15:30:24 +0500

    minimaze tour detail

--- src/app/tours/[id]/page.tsx
+++ src/app/tours/[id]/page.tsx
@@ -1,17 +1,3 @@
-import { notFound } from 'next/navigation';
-
-export default function TourPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const { id } = params;
-
-  if (!id) return notFound();
-
-  return (
-    <main className="p-6 bg-black text-white">
-      <h1>Тур #{id}</h1>
-    </main>
-  );
+export default function Page({ params }: { params: { id: string } }) {
+  return <main>Tour #{params.id}</main>;
 }
